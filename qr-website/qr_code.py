e7a48cc58352d775581f25fa74ef43b40648575a
Aisha Newland <aishamtnewland@gmail.com>
2025-12-26 19:13:36 -0600

    Add qr-website, templates, index.html

--- qr-website/qr_code.py
+++ qr-website/qr_code.py
@@ -2,9 +2,9 @@ import qrcode
 
 website_url = input("Enter the website URL you want to create a QR code for: ")
 
-version_number = input("Enter a number between 1-40 for the qr code verison: ")
-box_size_number = input("Enter a number between 1-10 for the pixels in each 'box' of the qr code: ")
-border_number = input("Enter a number between 1-10 for the border size of the qr code: ")
+
+box_size_number = int(input("Enter a number between 1-10 for the pixels in each 'box' of the qr code: "))
+border_number = int(input("Enter a number between 1-10 for the border size of the qr code: "))
 background_color = input("Enter a color name or hex code for the background color of the qr code: ")
 filler_color = input("Enter a color name or hex code for the filler color of the qr code: ")
 
